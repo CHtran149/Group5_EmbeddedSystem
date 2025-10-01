357ec288c669cbb5ee322e67d1036296fc1f1d03
CHtran149 <chienquangtran149@gmail,com>
2025-10-01 11:18:25 +0700

    update

--- EX12/Project/Objects/main.d
+++
@@ -1,7 +0,0 @@
-.\objects\main.o: main.c
-.\objects\main.o: ..\CMSIS\Device\STM32F10X\stm32f10x.h
-.\objects\main.o: ..\CMSIS\Core\core_cm3.h
-.\objects\main.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
-.\objects\main.o: ..\CMSIS\Device\STM32F10X\system_stm32f10x.h
-.\objects\main.o: ..\StdPeriph\inc\stm32f10x_rcc.h
-.\objects\main.o: ..\StdPeriph\inc\stm32f10x_gpio.h
--- EX12/Project/Objects/system_stm32f10x.d
+++
@@ -1,5 +0,0 @@
-.\objects\system_stm32f10x.o: RTE\Device\STM32F103C8\system_stm32f10x.c
-.\objects\system_stm32f10x.o: ..\CMSIS\Device\STM32F10X\stm32f10x.h
-.\objects\system_stm32f10x.o: ..\CMSIS\Core\core_cm3.h
-.\objects\system_stm32f10x.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
-.\objects\system_stm32f10x.o: ..\CMSIS\Device\STM32F10X\system_stm32f10x.h
